3b7bc057903ffd19c3a735a30a5a4d309785e3e8
Justus2004 <jus.bernard.23@lehre.mosbach.dhbw.de>
2023-12-21 10:40:20 +0100

    Funktionstüchtiges Galgenmännchen

--- Week_3/Galgenmaennchen.py
+++ Week_3/Galgenmaennchen.py
@@ -12,17 +12,47 @@ def eingabeBuchstaben():
                 temp = False 
         if(letter<=97 and letter>=122):
             print("Das Eingegebene war kein Buchstabe!")
+    letter =chr(letter)
     return letter
 
 def woerterQuelle():
     woerter =["subtrahieren", "sagen", "silikon", "kerzen", "antennen", "elastisch", "fallschirm","posieren", "steckdose", "kinderfrau", "torpedieren"]
-    temp=random.randint(0,len(woerter))
+    temp=random.randint(0,len(woerter)-1)
     char = list(woerter[temp])
     return char
-           
+
+def woerterLoesen():
+    count =0
+    inwort = False
+    wort = woerterQuelle()
+    wortlen = leerWort(wort)
+    while testFertig(wortlen):
+        count+=1
+        letter = eingabeBuchstaben()
+        x=0
+        for i in wort:
+            if letter == wort[x]:
+                wortlen[x] = letter
+                inwort = True
+            x+=1
+        if inwort:
+            print("Buchstabe vorhanden!")
+        else:
+            print("Buchstabe nicht vorhanden!")
+        print(wortlen)
+    print("Glückwunsch! Wort erraten nach ",count," Versuchen")
+            
+def testFertig(wortlen):
+    return "_" in wortlen
+
+def leerWort(wort):
+    wortlen =[]
+    for i in wort:
+        wortlen.append("_")
+    return wortlen
+        
 def main():
-    eingabeBuchstaben()
-    woerterQuelle()
+    woerterLoesen()
     
 if __name__ == "__main__":
     main()
